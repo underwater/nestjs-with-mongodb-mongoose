f5c0bedfc5e8db60ff64fb9e40c7be6ecb10f80f
Fares Eidi <fares.eidi@gmail.com>
2024-01-29 10:48:08 +0100

    add docker file for mongo and

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // If you want the config to be available in all modules
+    }),
     MongooseModule.forRoot('mongodb://127.0.0.1/nestjs_tutorial'),
     UsersModule,
     PostsModule,
