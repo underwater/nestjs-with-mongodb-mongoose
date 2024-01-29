52c56f0b25e8cee86b5236e5deb27db8c16d7091
Fares Eidi <fares.eidi@gmail.com>
2024-01-29 10:56:35 +0100

    add example http requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,31 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // If you want the config to be available in all modules
     }),
-    MongooseModule.forRoot('mongodb://127.0.0.1/nestjs_tutorial'),
+
+    // the usual way to import MongooseModule
+    // MongooseModule.forRoot(this.configService.get<string>('MONGO_URI'),
+
+    // to be able to use  factory function and inject the ConfigService... 
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     UsersModule,
     PostsModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+}
